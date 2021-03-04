2d8b26b1320acdafe71165bc42d630d95e188341
Chua Zi Heng <chuaziheng98@gmail.com>
2021-03-05 01:28:35 +0800

    Added age and sex detection

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import logging
 from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from keras.models import load_model
 from keras.preprocessing import image
+import tensorflow as tf
 
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template
@@ -23,9 +24,11 @@ app = Flask(__name__)
 
 # Model saved with Keras model.save()
 EMOTIONS_MODEL_PATH = 'models/emotion_model.hdf5'
+AGE_SEX_MODEL_PATH = 'models/Age_sex_detection.h5'
 
 # Load your trained model
-model = load_model(EMOTIONS_MODEL_PATH)
+emo_model = load_model(EMOTIONS_MODEL_PATH)
+agesex_model = load_model(AGE_SEX_MODEL_PATH)
 print('Model loaded. Start serving...')
 
 # You can also use pretrained model from Keras
@@ -40,24 +43,30 @@ print('Model loaded. Start serving...')
 
 #---end---
 emotion_dict= {'Angry': 0, 'Sad': 5, 'Neutral': 4, 'Disgust': 1, 'Surprise': 6, 'Fear': 2, 'Happy': 3}
+gender_dict = { 0:'Male', 1:'Female'}
 
-def model_predict(img_path, model):
-    img = image.load_img(img_path, target_size=(48, 48),color_mode='grayscale')
-
+def model_predict(img_path, emo_model, agesex_model):
+    img = image.load_img(img_path, target_size=(48, 48))
+    img_grayscale = image.load_img(img_path, target_size=(48, 48), color_mode='grayscale')
     # Preprocessing the image
-    img = image.img_to_array(img)  # (48,48,1)
+    img = image.img_to_array(img)  # (48,48,3)
+    img_grayscale = image.img_to_array(img_grayscale) #(48.48,1)
     # x = np.true_divide(x, 255)
-    img = np.expand_dims(img, axis=0) #(1,48,48,1)
-
+    img = np.expand_dims(img, axis=0) #(1,48,48,3)
+    img_grayscale = np.expand_dims(img_grayscale, axis=0) #(1,48,48,1)
+    img_norm = img/255
     # Be careful how your trained model deals with the input
     # otherwise, it won't make correct prediction!
     # x = preprocess_input(x, mode='caffe')
     # preds = model.predict(x)
-    predicted_class = np.argmax(model.predict(img))
+    emotion_class = np.argmax(emo_model.predict(img_grayscale))
     label_map = dict((v,k) for k,v in emotion_dict.items())
-    preds = label_map[predicted_class]
-
-    return preds
+    emotion_preds = label_map[emotion_class]
+    preds = agesex_model.predict(img_norm)
+    gender = gender_dict[int(np.round(preds[0][0][0]))]
+    age = int(np.round(preds[1][0][0]))
+    result_tuple = (emotion_preds, gender, age)
+    return str(result_tuple) #emotion_preds, gender ,
 
 
 @app.route('/', methods=['GET'])
@@ -78,8 +87,9 @@ def upload():
             basepath, 'uploads', secure_filename(f.filename))
         logging.info(f'image file path {file_path}')
         f.save(file_path)
-        result = model_predict(file_path,model)
-        return result
+        result = model_predict(file_path,emo_model, agesex_model)
+        logging.info(result)
+        return result  #(emotion, gender, age)
         # Make prediction
         # preds = model_predict(file_path, model)
 
